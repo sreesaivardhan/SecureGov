e8f7f2d446f27377e62f772b48b664e5bbfdc0f9
sreesaivardhan <sreesaivardhans@gmail.com>
2026-06-09 02:05:22 +0530

    Update production backend URL

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@
 window.API_BASE_URL = (
   window.location.hostname === 'localhost' ||
   window.location.hostname === '127.0.0.1'
-) ? 'http://localhost:5000' : 'https://securegov-backend.onrender.com';
+) ? 'http://localhost:5000' : 'https://securegov.onrender.com';
